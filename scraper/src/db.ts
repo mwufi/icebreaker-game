9c58e442704e20288639273bdada6c3297f8d882
Zen T <ztang230@gmail.com>
2025-09-05 15:18:18 -0400

    GET /profiles

--- scraper/src/db.ts
+++ scraper/src/db.ts
@@ -1,5 +1,6 @@
 import { Database } from "bun:sqlite";
 import { drizzle } from "drizzle-orm/bun-sqlite";
+import * as schema from "./schema";
 
 export const sqlite = new Database("profiles.db");
-export const db = drizzle(sqlite);
+export const db = drizzle(sqlite, { schema });
